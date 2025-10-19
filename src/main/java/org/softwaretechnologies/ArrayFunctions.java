84f0c0589f8c8eb863d0ff082190b3cb614d69fd
lazor <lazorcevd@gmai.com>
2025-10-19 23:05:22 +0300

    Task 1

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -10,25 +10,53 @@ public final class ArrayFunctions {
      * @param array массив, который будет перевернут.
      */
     public static void reverse(int[] array) {
-        // TODO: реализуйте вышеуказанную функцию
-        for(int x=0; x< array.length/2; x++){
-            int y=array[x];
-            array[x]=array[array.length-1-x];
-            array[array.length-1-x]=y;
+        if (array == null){
+            return;
+        }
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right){
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
         }
     }
 
     /**
      * Функция, заменяющая строки матрицы на столбцы матрицы. Пример:
-       1  2  3     1  4  7
-       4  5  6     2  5  8
-       7  8  9     3  6  9
+     1  2  3     1  4  7
+     4  5  6     2  5  8
+     7  8  9     3  6  9
      * Функция работает только с квадратными матрицами.
      * Если матрица не квадратная, то выведете на экран сообщение:
-       Матрица не квадратная
+     Матрица не квадратная
      * @param matrix матрица, в которой столбцы будут заменены на строки.
      */
     public static void rotateMatrix(int[][] matrix) {
-        // TODO: реализуйте вышеуказанную функцию
+        if (matrix == null) {
+            System.out.println("Матрица не квадратная");
+            return;
+        }
+
+        // Проверяем является ли матрица квадратной
+        int n = matrix.length;
+
+        if (n == 0 | matrix[0] == null | matrix[0].length != n) {
+            System.out.println("Матрица не квадратная");
+            return;
+        }
+
+        // Транспонируем матрицу
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                // Меняем местами элементы
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
     }
 }
